8b5580fc6ec66511413e597fdcf5e8939fc4e599
hothanhluan <hothanhluan1996@gmail.com>
2017-10-30 08:54:02 +0700

    2nd commit

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ let app = express();
 app.get('/api', function(req, res) {
     let msg = req.query.msg;
     let source = req.query.source;
+    let userid = req.querry.userid;
+    let email = req.querry.email;
+    let sdt = req.querry.sdt;
+    let sothich = req.querry.sothich;
 
     let phanhoi = {
         "messages": [
@@ -13,6 +17,21 @@ app.get('/api', function(req, res) {
         ]
     };
     
+    let luudulieuuser = {
+	"root" : [
+		{"name" : ""},	
+		{"email " : ""},
+		{"sdt" : ""},
+		{"id" : ""}, 
+		{"sothich" : ""}
+		]
+	};
+	
+	luudulieuuser.root['id'] = userid;
+	luudulieuuser.root['email'] = email;
+	luudulieuuser.root['sdt'] = sdt;
+	luudulieuuser.root['sothich'] = sothich;
+	
     if (msg == 'hello') {
         phanhoi.messages[0].text = 'hi there';
         res.end(JSON.stringify(phanhoi));
@@ -29,4 +48,7 @@ app.get('/api', function(req, res) {
     res.end(JSON.stringify(phanhoi));
 });
 
+
 app.listen(process.env.PORT || 8080);
+
+
